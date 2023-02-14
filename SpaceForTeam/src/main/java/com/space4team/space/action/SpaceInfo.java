8d4c5f71ec3e7f8d6745c8f0207678d8ee242726
user <user@DESKTOP-2PGRMF4>
2023-02-14 17:49:21 +0900

    SpaceInfo finish & Original

---
+++ SpaceForTeam/src/main/java/com/space4team/space/action/SpaceInfo.java
@@ -0,0 +1,29 @@
+package com.space4team.space.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.space4team.db.SpaceDAO;
+import com.space4team.db.SpaceDTO;
+
+public class SpaceInfo implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("SpaceInfo execute()");
+		int s_num=1;
+//				Integer.parseInt(request.getParameter("s_num"));
+		
+		SpaceDAO dao=new SpaceDAO();
+		SpaceDTO dto=dao.getSpace(s_num);
+		
+		
+		request.setAttribute("dto", dto);
+		
+		ActionForward forward=new ActionForward();
+		forward.setPath("space/info.jsp");
+		forward.setRedirect(false);
+		return forward;
+	}
+
+}
