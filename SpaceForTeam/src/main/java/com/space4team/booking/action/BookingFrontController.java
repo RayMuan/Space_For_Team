01bd1d4dad5b1ea035600b82fcff624021972ca8
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-27 16:05:32 +0900

    booking 경로수정

--- SpaceForTeam/src/main/java/com/space4team/booking/action/BookingFrontController.java
+++ SpaceForTeam/src/main/java/com/space4team/booking/action/BookingFrontController.java
@@ -60,13 +60,6 @@ public class BookingFrontController extends HttpServlet{
 				e.printStackTrace();
 			}
 			
-		}else if(sPath.equals("/BookingContent.bk")) {
-			action = new BookingContent();
-			try {
-				forward = action.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}else if(sPath.equals("/BookingUpdateForm.bk")) {
 			action = new BookingUpdateForm();
 			try {
@@ -91,22 +84,17 @@ public class BookingFrontController extends HttpServlet{
 		}else if(sPath.equals("/Home.bk")) {
 			System.out.println("/Home.bk");
 			forward = new ActionForward();
-			forward.setPath("main/user.jsp");
-			forward.setRedirect(false);
-		}else if(sPath.equals("/Home.bk")) {
-			System.out.println("/Home.bk");
-			forward = new ActionForward();
-			forward.setPath("main/user.jsp");
+			forward.setPath("MainPro.sp");
 			forward.setRedirect(false);
 		}else if(sPath.equals("/LoginForm.bk")) {
 			System.out.println("/LoginForm.bk");
 			forward = new ActionForward();
-			forward.setPath("user/contact.jsp");
+			forward.setPath("UserLoginForm.us");
 			forward.setRedirect(false);
 		}else if(sPath.equals("/InsertForm.bk")) {
 			System.out.println("/InsertForm.bk");
 			forward = new ActionForward();
-			forward.setPath("join/userInsert.jsp");
+			forward.setPath("UserInsertForm.us");
 			forward.setRedirect(false);
 		}
 			
